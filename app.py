4729f1d116286b5c616b3148b3fec978a0bde026
Akansha <111658205+Akansha1409@users.noreply.github.com>
2025-11-20 12:38:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -142,4 +142,4 @@ def chat():
 # 9. Run Flask app
 # -----------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
